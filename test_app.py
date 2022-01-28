0c2e7b017050ef889b6e2a4495df30b8de59db9b
Lidan-Huang <huangld0314@gmail.com>
2022-01-28 12:37:47 -0800

    finished add test of checking html in homepage

--- test_app.py
+++ test_app.py
@@ -25,6 +25,9 @@ class BoggleAppTestCase(TestCase):
             response = client.get('/')
             ...
             # test that you're getting a template
+            html = response.get_data(as_text=True)
+
+            self.assertIn('<button class="word-input-btn">Go</button>', html)
 
     def test_api_new_game(self):
         """Test starting a new game."""
